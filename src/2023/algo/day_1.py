72e88c711b98127576ee7f5863c3ff3fa7a6b873
Mathias Gesbert <mathias.gesbert@doctolib.com>
2023-12-02 18:40:31 +0100

    2023 day 1

---
+++ src/2023/algo/day_1.py
@@ -0,0 +1,44 @@
+def part_1(input_data):
+    return sum(
+        int(
+            next(c for c in line if c.isdigit())
+            + next(c for c in line[::-1] if c.isdigit())
+        )
+        for line in input_data
+    )
+
+
+def replace_digits(line):
+    translations = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    res = ""
+    for i, c in enumerate(line):
+        word = next((k for k in translations.keys() if line[i:].startswith(k)), None)
+        if word:
+            res += translations[word]
+            for _ in range(len(word) - 1):
+                next(c for c in line)
+        else:
+            res += c
+
+    return res
+
+
+def part_2(input_data):
+    return sum(
+        int(
+            next(c for c in replace_digits(line) if c.isdigit())
+            + next(c for c in replace_digits(line)[::-1] if c.isdigit())
+        )
+        for line in input_data
+    )
